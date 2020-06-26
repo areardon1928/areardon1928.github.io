3e10a6b4458f35ffd6e0af9986fa91b5f00f88a2
areardon1928 <44825818+areardon1928@users.noreply.github.com>
2020-06-26 09:30:04 -0400

    Add files via upload

---
+++ home/table.js
@@ -0,0 +1,17 @@
+angular.module('phonecatApp').component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
+    template: '<ul></ul>',
+    controller: function PhoneListController() {
+      this.phones = [
+        {
+          name: 'Nexus S',
+          snippet: 'Fast just got faster with Nexus S.'
+        }, {
+          name: 'Motorola XOOM™ with Wi-Fi',
+          snippet: 'The Next, Next Generation tablet.'
+        }, {
+          name: 'MOTOROLA XOOM™',
+          snippet: 'The Next, Next Generation tablet.'
+        }
+      ];
+    }
+  });
